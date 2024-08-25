a6dd83420439f628361abca81dcd1281d638ac1a
ajcaesar <ajcaesar294@gmail.com>
2024-08-25 16:04:27 -0400

    changed backend cors config

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 
 const app = express();
 
-// app.use(cors({
-//     origin: ['https://example.com', 'https://another-example.com']
-//   }));
-app.use(cors());
+app.use(cors({
+    origin: ["https://statjack2.onrender.com"]
+  }));
+// app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
